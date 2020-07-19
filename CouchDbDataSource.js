f59bf0cf422ede9cf4033ec6c5440555a3733f2b
Andrew Zigler <andrewzigler@gmail.com>
2020-07-18 22:07:29 -0700

    Clean up comments

--- CouchDbDataSource.js
+++ CouchDbDataSource.js
@@ -7,16 +7,6 @@ const DB_PW = process.env.DB_PW
  * DataSource using CouchDB
  */
 module.exports = class CouchDbDataSource {
-  /**
-   * This is the only required method of a DataSource, all others are optional
-   * but not implementing them will obviously limit its funtionality.
-   *
-   * Returns whether or not there is data for a given config. In the case of the
-   * YamlDataSource this would be whether or not the configured file exists
-   *
-   * @param {object} config
-   * @return {Promise<boolean>}
-   */
   hasData (config = {}) {
     const db = new PouchDB(`http://${DB_UN}:${DB_PW}@localhost:5984/${config.namespace}${config.db}`)
     let result = false
@@ -24,20 +14,14 @@ module.exports = class CouchDbDataSource {
     return result
   }
 
-  /**
-   * Returns all entries for a given config.
-   *
-   * @param {object} config
-   * @return {Promise<any>}
-   */
-  async fetchAll (config = {}) {
+  fetchAll (config = {}) {
     const db = new PouchDB(`http://${DB_UN}:${DB_PW}@localhost:5984/${config.namespace || ''}${config.db}`)
 
-    // if loading a file belonging to an area
+    // if loading a document belonging to an area
     // (npcs, items, rooms, and quests)
     // RETURNS AN ARRAY
     if (config.area) {
-      return await db.allDocs({
+      return db.allDocs({
         include_docs: true,
         startkey: config.area,
         endkey: config.area
@@ -47,11 +31,11 @@ module.exports = class CouchDbDataSource {
         })
         .catch(er => { return er })
 
-    // if loading a non-area file
+    // if loading a non-area document
     // (accounts, players, and help)
     // RETURNS AN OBJ
     } else {
-      return db.allDocs({
+      return await db.allDocs({
         include_docs: true,
         startkey: config.area,
         endkey: config.area
@@ -69,13 +53,6 @@ module.exports = class CouchDbDataSource {
     }
   }
 
-  /**
-   * Gets a specific record by id for a given config
-   *
-   * @param {Object} config
-   * @param {string} id
-   * @return {Promise<any>}
-   */
   fetch (config = {}, id) {
     const db = new PouchDB(`http://${DB_UN}:${DB_PW}@localhost:5984/${config.namespace}${config.db}`)
     return db
@@ -84,14 +61,6 @@ module.exports = class CouchDbDataSource {
       .catch(er => { return er })
   }
 
-  /**
-   * Update specific record. Write version of `fetch`
-   *
-   * @param {Object} config
-   * @param {string} id
-   * @param {any} data
-   * @return {Promise}
-   */
   update (config = {}, id, payload) {
     const db = new PouchDB(`http://${DB_UN}:${DB_PW}@localhost:5984/${config.namespace}${config.db}`)
     return this.fetch(config, id)
